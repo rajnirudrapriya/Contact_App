e8c7833338cfab09c9a651e1454599c43b601e3c
rajnirudrapriya <rajnirudrapriya@gmail.com>
2025-06-19 08:36:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const app=express()
+require('dotenv').config();
+const {connectDb}=require("./config/db")
+const routing=require("./router/contactRouter");
+const {engine}=require('express-handlebars')
+let PORT=process.env.PORT;
+
+app.engine('handlebars', engine());
+app.set('view engine','handlebars');
+
+connectDb();
+
+app.use(express.urlencoded({extended:true}));
+app.use("/api", routing);
+app.use(express.static("public"));
+
+app.get('/', (req,res)=>{
+    res.render('home', {title: "homepage", css:"home"});
+})
+
+app.listen(PORT, err=>{
+    if(err) throw err
+    console.log("Server is running on port 5000");    
+})
