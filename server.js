416e5ce858dd01eff985ebba1afe4a0c134fe82a
rajnirudrapriya <rajnirudrapriya@gmail.com>
2025-06-19 09:17:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express=require('express')
-const app=express()
+const express = require('express');
+const app = express();
 require('dotenv').config();
-const {connectDb}=require("./config/db")
-const routing=require("./router/contactRouter");
-const {engine}=require('express-handlebars')
-let PORT=process.env.PORT;
+const { connectDb } = require("./config/db");
+const routing = require("./router/contactRouter");
+const { engine } = require('express-handlebars');
+let PORT = process.env.PORT;
 
 app.engine('handlebars', engine());
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
 
 connectDb();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", routing);
 app.use(express.static("public"));
 
-app.get('/', (req,res)=>{
-    res.render('home', {title: "homepage", css:"home"});
-})
+app.get('/', (req, res) => {
+    res.render('home', { title: "homepage", css: "home" });
+});
 
-app.listen(PORT, err=>{
-    if(err) throw err
-    console.log("Server is running on port 5000");    
-})
+// ❌ REMOVE THIS PART 👇
+// app.listen(PORT, err => {
+//     if (err) throw err;
+//     console.log("Server is running on port 5000");
+// });
+
+// ✅ ADD THIS INSTEAD 👇
+module.exports = app;
