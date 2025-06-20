61f5f188f78c8962d5983b47a7c4400fd719da5d
rajnirudrapriya <rajnirudrapriya@gmail.com>
2025-06-20 09:35:17 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../server'); // server.js ko import kiya
+const app = require('../server');
 
 module.exports.handler = serverless(app);
